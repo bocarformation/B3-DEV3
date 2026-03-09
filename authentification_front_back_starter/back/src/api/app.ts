f886887c1dfb45d4fd2f47531894f86e23e5acd7
bocarformation <bocsco3@gmail.com>
2026-03-09 15:25:27 +0100

    add config for helmet and cors

--- authentification_front_back_starter/back/src/api/app.ts
+++ authentification_front_back_starter/back/src/api/app.ts
@@ -4,9 +4,16 @@ import { errorHandlerMiddleware } from './middlewares/error-handler.middleware';
 import { jsonApiResponseMiddleware } from './middlewares/json-response.middleware';
 import { authRouter } from './routes/auth.routes';
 import { projectRouter } from './routes/project.routes';
+import helmet from 'helmet';
+import cors from "cors";
 
 const app = express();
 
+app.use(helmet());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 app.use(morgan("dev"))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
